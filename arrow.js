a12b8c706ddea40ed4e3263cb6c5e043aad2c3f0
MostafaShelim <mostafarmstu@gmail.com>
2020-08-30 20:13:40 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,19 @@
+// const doubleIt = function(num){
+//     return 2*num;
+// }
+// const result = doubleIt(5);
+// console.log(result);
+
+// const doubleIt = function myfun(num){
+//     return 2*num;
+// }
+// const result = doubleIt(5);
+// console.log(result);
+
+// const doubleIt = num => 2*num;
+// const result = doubleIt(5);
+// console.log(result);
+
+const add = (num1, num2) => num1 + num2;
+const result = add(5, 5);
+console.log(result);
